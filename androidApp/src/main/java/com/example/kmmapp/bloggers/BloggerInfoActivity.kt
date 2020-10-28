bf010f3f43050a41af84f3ea089f9cefb1f8c44c
Deniz <deniz.vaisov@gmail.com>
2020-10-28 18:59:08 +0300

    Make Blogger serializable and working with bloggers UI

--- androidApp/src/main/java/com/example/kmmapp/bloggers/BloggerInfoActivity.kt
+++ androidApp/src/main/java/com/example/kmmapp/bloggers/BloggerInfoActivity.kt
@@ -1,46 +1,43 @@
 package com.example.kmmapp.bloggers
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
-import com.example.kmmapp.androidApp.MainActivity
 import com.example.kmmapp.androidApp.R
 
 class BloggerInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blogger_info)
-        getBloggerInfo(BloggerActivity().bloggers)
+        getBloggerInfo()
     }
 
     fun setImageURL(blogger: Blogger, imageView: ImageView) {
-        val imgUrl: String = intent.getStringExtra("image-url").toString()
         val media = blogger.imageURL
+
         Glide.with(this)
-            .load(imgUrl)
+            .load(media)
             .into(imageView)
     }
 
-    fun getBloggerInfo(bloggers: MutableList<Blogger>){
-        val nick: String = intent.getStringExtra("nickName").toString()
+    @SuppressLint("SetTextI18n")
+    fun getBloggerInfo(){
+        val bloggers = intent.getSerializableExtra("bloggers") as Blogger
 
-        val imageView: ImageView =     findViewById(R.id.blogger_img_info)
+        val imageView: ImageView =    findViewById(R.id.blogger_img_info)
         val nickName: TextView =      findViewById(R.id.blogger_nick_name_info)
         val realName: TextView =      findViewById(R.id.blogger_real_name_info)
         val age: TextView =           findViewById(R.id.blogger_age_info)
         val description: TextView =   findViewById(R.id.blogger_description_info)
 
-        for (blogger : Blogger in bloggers) {
-            setImageURL(blogger, imageView)
-            nickName.text = nick
-            realName.text = blogger.realName
-            age.text = blogger.age.toString()
-            description.text = blogger.description
-        }
+        setImageURL(bloggers, imageView)
+        nickName.text = "Псевдоним:" + "\n" + bloggers.nickName
+        realName.text = "Имя:" + "\n" + bloggers.realName
+        age.text = "Возвраст:" + "\n" + bloggers.age.toString() + " лет"
+        description.text = "Биография:" + "\n" + bloggers.description
     }
 }
+
